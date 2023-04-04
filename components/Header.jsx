4ebd94809c9c9ac9f69e04de065ab1a46cd9537c
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-04-04 01:56:07 -0400

    temp

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
 									<div className="col-md-6 mt-2">
 										<h2>Oscar Alcázar Lobos</h2>
 										<h4>Ingeniero en Informática</h4>
-										<p className="text-muted"> Personalidad: INTP-A </p>
+										{/* <p className="text-muted"> Personalidad: INTP-A </p> */}
 									</div>
 									<div className="col-md-6">
 										<p className="text-muted"> Correo: oscar.alcazar.lobos@gmail.com </p>
@@ -26,9 +26,9 @@ const Header = () => {
 								</div>
 								<hr />
 								<p>
-								Trabajador respetuoso, adaptable a distintos grupos de trabajo y situaciones de presión y/o relajo, empático con sus compañeros, responsable, puntual y apasionado.
-Soy una persona tranquila, a la que le gusta leer, aprender, salir y compartir con amigos y familia, puedo comunicarme fácilmente con la gente, lo que me ayuda a crear lazos tanto profesionales como personales. Cuento con metas
-claras, con la motivación y perseverancia para cumplir cada una de ellas.
+								Persona respetuosa, con muchas ganas de seguir aprendiendo y certificarme en las distintas tecnologías que mi puesto requiera para poder entregar lo mejor de mí. Soy adaptable a distintos grupos de trabajo y/o situaciones de presión y/o relajo. 
+Cuento con experiencia en varios entornos de desarrollo, siendo Javascript, Typescript, React y Node, actualmente mi mayor fortaleza. Tengo la capacidad de crear proyectos Frontend y Backend. Conectarlos a través de APIs y desarrollarlos con código limpio, ordenado y con principios de programación SOLID.
+
 
 								</p>
 							</div>
